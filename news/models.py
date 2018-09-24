913e6b22b4eebba4d390a118fee53c9638f8f48b
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-09-24 15:29:55 +0300

    second commit

--- news/models.py
+++ news/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Editor(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+    try:
+        editor = Editor.objects.get(email = 'moha@gmail.com')
+        print('Editor found')
+    except DoesNotExist:
+        print('Editor was not found')
